9760ecbb4ef81a820a8159ba70ba7ba36fb74320
Robert Widmann <devteam.codafi@gmail.com>
2015-01-21 22:22:40 -0700

    Subtree Split

---
+++ Swiftx/Swiftx.h
@@ -0,0 +1,19 @@
+//
+//  Swiftx.h
+//  Swiftx
+//
+//  Created by Robert Widmann on 1/21/15.
+//  Copyright (c) 2015 TypeLift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swiftx.
+FOUNDATION_EXPORT double SwiftxVersionNumber;
+
+//! Project version string for Swiftx.
+FOUNDATION_EXPORT const unsigned char SwiftxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftx/PublicHeader.h>
+
+
--- SwiftxTests/SectionTests.swift
+++ SwiftxTests/SectionTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-import swiftz_core
+import Swiftx
 
 class SectionTests: XCTestCase {
 	func testShiftRightSections() {
