1c6dde26b94f8589fbd691c38d4ad682cca15fb3
Robert Widmann <devteam.codafi@gmail.com>
2015-07-15 12:22:10 -0600

    Add `on`

--- Swiftx/Combinators.swift
+++ Swiftx/Combinators.swift
@@ -92,3 +92,45 @@ public func |> <A, B>(a : A, f: A -> B) -> B {
 public func fix<A>(f : ((A -> A) -> A -> A)) -> A -> A {
 	return { x in f(fix(f))(x) }
 }
+
+/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// left to the result of both prior applications.
+///
+///    f |*| g = { x in { y in f(g(x))(g(y)) } }
+///
+/// This function may be useful when a comparing two like objects using a given property, as in:
+///
+///     let arr : [(Int, String)] = [(2, "Second"), (1, "First"), (5, "Fifth"), (3, "Third"), (4, "Fourth")]
+///     let sortedByFirstIndex = arr.sort((<) |*| fst)
+public func |*| <A, B, C>(o : B -> B -> C, f : A -> B) -> A -> A -> C {
+	return on(o)(f)
+}
+
+/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// left to the result of both prior applications.
+///
+///    (+) |*| f = { x, y in f(x) + f(y) }
+///
+/// This function may be useful when a comparing two like objects using a given property, as in:
+///
+///     let arr : [(Int, String)] = [(2, "Second"), (1, "First"), (5, "Fifth"), (3, "Third"), (4, "Fourth")]
+///     let sortedByFirstIndex = arr.sort((<) |*| fst)
+public func |*| <A, B, C>(o : (B, B) -> C, f : A -> B) -> A -> A -> C {
+	return on(o)(f)
+}
+
+/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// left to the result of both prior applications.
+///
+///    (+) |*| f = { x in { y in f(x) + f(y) } }
+public func on<A, B, C>(o : B -> B -> C) -> (A -> B) -> A -> A -> C {
+	return { f in { x in { y in o(f(x))(f(y)) } } }
+}
+
+/// On | Applies the function on its right to both its arguments, then applies the function on its
+/// left to the result of both prior applications.
+///
+///    (+) |*| f = { x, y in f(x) + f(y) }
+public func on<A, B, C>(o : (B, B) -> C) -> (A -> B) -> A -> A -> C {
+	return { f in { x in { y in o(f(x), f(y)) } } }
+}
