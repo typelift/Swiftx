a51e28dcee99d86931465170177f000a3afe645c
Robert Widmann <devteam.codafi@gmail.com>
2015-08-08 13:25:54 -0600

    Remove false caveat clause

--- Swiftx/Error.swift
+++ Swiftx/Error.swift
@@ -22,8 +22,6 @@ public func error<A>(x : String) -> A {
 ///     public func sortBy<A>(cmp : (A, A) -> Bool)(l : [A]) -> [A] {
 ///         return undefined()
 ///     }
-///
-/// The same caveat about compilation modes applies to "undefined" as to "error".
 public func undefined<A>() -> A {
 	return error("Undefined")
 }
